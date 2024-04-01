f492ec979dada5a7bccfc68eaf3e51d8e6f27744
amirzz <B022210171@student.utem.edu.my>
2024-04-02 03:02:59 +0800

    amir

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+   res.send('amir busuk')
+})
+
+app.listen(port, () => {
+   console.log(`Example app listening on port ${port}`)
+})
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://b022210171:1Ii2I9ORdxU0AV8G@cluster0.1x17rsv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
